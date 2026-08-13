420bd7dc37efa54f0db9997edc30cb06133580f4
basilmursi66-beep <basilmursi66@gmail.com>
2026-08-13 15:33:11 +0000

    tutorial_6

---
+++ week_6/employee.java
@@ -0,0 +1,19 @@
+package week_6;
+
+public class employee {
+    
+
+    protected String id;
+    protected String name;
+
+    public employee(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void displayInfo() {
+        System.out.println("Employee ID : " + id);
+        System.out.println("Name     : " + name);
+    }
+}
+
---
+++ week_6/main.java
@@ -0,0 +1,12 @@
+package week_6;
+
+public class main {
+    
+    public static void main(String[] args) {
+        lecturer l = new lecturer("L101", "Dr Basel","OOP", "Faculty of Information Technology");
+        
+        l.displaySubject();
+        l.displayDepartment();
+    }
+}
+
